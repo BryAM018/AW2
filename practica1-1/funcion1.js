65f06dbbefa368b68fa8d1ade97134b3ec6fe77c
bjam1315074052 <bjam1315074052@gmail.com>
2023-04-14 20:00:23 -0500

    menos codigo en trabajo1 y mas entendible

--- practica1-1/funcion1.js
+++ practica1-1/funcion1.js
@@ -1,6 +1,6 @@
 const { mostrarVehiculos , mostrarEspaciosDeParqueo, mostrarParqueos }  = require('./practica1');
-//Vinculo con el otro archivo
-//const functions= require('./practica1')
+
+
 
 //Mostrar por consola
 
