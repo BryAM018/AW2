8f6fb0ea911dde6b8e509cf04feaec4f85a746d6
BryAM018 <bjam1315074052@gmail.com>
2023-04-28 09:14:28 -0500

    CRud

--- practica3-1/index.js
+++ practica3-1/index.js
@@ -1,3 +1,4 @@
+//Conectarse a su propia base de datos con MongoDB Atla
 const mongoose= require('mongoose');
 const cadenaConexion= "mongodb+srv://bryam018:RbVP1A4RBo42WXhQ@cluster0.0ggvdtd.mongodb.net/practica";
 
@@ -5,14 +6,27 @@ const cadenaConexion= "mongodb+srv://bryam018:RbVP1A4RBo42WXhQ@cluster0.0ggvdtd.
 
     try {
         const stateConnection= await mongoose.connect(cadenaConexion);
-        const Vehiculos = mongoose.model("vehiculo",{ descripcion: 'String', placa: 'String', color: 'String' })
-        const EspaciosDeParqueo = mongoose.model("espacio",{ descripcion: 'String' })
-        const Parqueos = mongoose.model("parqueo",{ 
+    //Definir tres modelos aplicando Mongoose a partir de sus entidades asignadas.
+        const Vehiculo = mongoose.model("vehiculo",{ descripcion: 'String', placa: 'String', color: 'String' });
+        const EspaciosDeParqueo = mongoose.model("espacio",{ descripcion: 'String' });
+    //relaciones foráneas existentes
+        const Parqueo = mongoose.model("parqueo",{ 
             vehiculoId: { type: mongoose.Types.ObjectId, ref:"Vehiculos" },
-            espacioDeParqueoId: { type: mongoose.Types.ObjectId, ref:"EspaciosDeParqueo" }
-            ,descripcion: 'String', fechaHoraEntrada: 'Date', fechaHoraSalida: 'null' })
-        
-
+            espacioDeParqueoId: { type: mongoose.Types.ObjectId, ref:"EspaciosDeParqueo" },
+            fechaHoraEntrada: 'Date', fechaHoraSalida: 'null' });
+        //Crear el CRUD de las entidades maestras para constatar la conexión.
+        const vehiculo1 =  new Vehiculo({ descripcion: 'Toyota Corolla', placa: 'ABC123', color: 'Rojo' });
+        const saveVehiculo = await  vehiculo1.save();
+        const espacio1 =  new EspaciosDeParqueo({ descripcion: 'Estacionamiento #1'});
+        const saveEspacio = await  espacio1.save();
+            
+        const parqueo1=  new Parqueo(
+            {
+            vehiculoId: saveVehiculo,
+            espacioDeParqueoId: saveEspacio,
+            fechaHoraEntrada: new Date(), fechaHoraSalida: 'null'
+            });
+        const saveParqueo= await parqueo1.save();
         
     } catch (error) {
         console.log(error);
